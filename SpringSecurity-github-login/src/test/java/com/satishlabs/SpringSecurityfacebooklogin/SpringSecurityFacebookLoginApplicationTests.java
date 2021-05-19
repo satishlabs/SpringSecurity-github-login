655c51a043ce40d00570229e362219e81ce10536
Satish Prasad <spd61185@gmail.com>
2021-05-19 07:58:53 +0530

    SpringSecurity-github-login

---
+++ SpringSecurity-github-login/src/test/java/com/satishlabs/SpringSecurityfacebooklogin/SpringSecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs.SpringSecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
